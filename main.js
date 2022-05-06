bc42b5a4f2352a2c6946d7c35aef8b12f359cbc4
psyopus69 <65310189+psyopus69@users.noreply.github.com>
2022-05-06 10:16:56 +0300

    Add files via upload
    Алексей Сергеевич commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let searchPanel = document.createElement('div');
-let searchBtn = document.createElement('div');
+let searchBtn = document.createElement('input');
 let answerDiv = document.createElement('div');
 let answerBlock = document.createElement('div');
-let clearBtn = document.createElement('div');
+let clearBtn = document.createElement('input');
 const input = document.createElement("input");
 
 let loadTreads = [];
@@ -15,21 +15,24 @@ function ready() {
     searchPanel.setAttribute("id", "mainSearchPanel");
 
     searchBtn.setAttribute("id", "mainSearchBtn");
-    searchBtn.innerHTML = "Поиск";
-	
-	
-	clearBtn.setAttribute("id", "clearBtn");
-    clearBtn.innerHTML = "Очистить";
-    clearBtn.addEventListener("click", () => {onClear();});
-	
+    searchBtn.type = 'button';
+    searchBtn.value = "Поиск";
+
+
+    clearBtn.setAttribute("id", "clearBtn");
+    clearBtn.type = 'button';
+    clearBtn.value = "X";
+    clearBtn.addEventListener("click", () => { onClear(); });
+
     answerBlock.innerHTML = "Результаты поиска:";
     answerBlock.setAttribute("id", "answer");
-	
-	answerBlock.appendChild(answerDiv);
 
-    searchBtn.addEventListener("click", () => {onSearchClick(input.value);});
+    answerBlock.appendChild(answerDiv);
+
+
+
+    searchBtn.addEventListener("click", () => { onSearchClick(input.value); });
     searchBtn.classList.add("searchBlock");
-
     input.setAttribute("id", "mainInput");
     input.type = "text";
     input.name = "SEARCH_INPUT";
@@ -43,47 +46,51 @@ function ready() {
     startAll();
 }
 
+
+
 function onClear() {
-	answerDiv.innerHTML = "";
-	input.value = "";
-	}
+    answerDiv.innerHTML = "";
+    input.value = "";
+}
 
 function onSearchClick(substring) {
     if (!enableSearch) return;
-	let counter = loadTreads.length;
-	answerDiv.innerHTML = "";
-	//substring = substring.replaceAll(" ", "%20");
+    let counter = loadTreads.length;
+    answerDiv.innerHTML = "";
+    //substring = substring.replaceAll(" ", "%20");
     for (let i = 0; i < loadTreads.length; i++) {
-       // console.log("\r\n\r\nURL: " + loadTreads[i][1] + "\r\n" + substring.toUpperCase() + "  \r\nIN (i: " + i + ")\r\n " + loadTreads[i][2].toUpperCase());
+        // console.log("\r\n\r\nURL: " + loadTreads[i][1] + "\r\n" + substring.toUpperCase() + "  \r\nIN (i: " + i + ")\r\n " + loadTreads[i][2].toUpperCase());
         if (loadTreads[i][3].toUpperCase().includes(substring.toUpperCase())) {
-       //     console.log("нашлось! " + substring + " по адресу" + loadTreads[i][1] + ", orig: " + loadTreads[i][2]);		  
-            answerDiv.innerHTML += "<div>" + loadTreads[i][2] + " (" + loadTreads[i][1] + ")</div>";
+            //console.log("нашлось! " + substring + " по адресу" + loadTreads[i][1] + ", orig: " + loadTreads[i][2]);
+            answerDiv.innerHTML += '<div class="block-link"><a class="link" href="'+window.location.href + encodeURI(loadTreads[i][1].replace(/%20/g, '+')) + "\r\n" + '"target="_blank">' + loadTreads[i][2] + ' (' + loadTreads[i][1] + ')</a></div>';
             answerBlock.appendChild(answerDiv);
         }
     }
 
 }
 
+
 const parseAllUrlds = (documentText) => {
     documentText = documentText.getElementsByTagName("td");
+    //console.log(documentText);
     let massHtml = "";
     for (let i = 0; i < documentText.length; i++) {
         if (documentText[i].innerHTML.includes("-->")) continue;
         massHtml += documentText[i].innerHTML;
-        // console.log(documentText[i].innerHTML);
+        //console.log(documentText[i].innerHTML);
     }
     let str = "<script>";
     const allSearchedBtns = massHtml.split(str + "btnL");
     // console.log(allSearchedBtns);
     for (let i = 0; i < allSearchedBtns.length; i++) {
-      //  console.log(allSearchedBtns[i]);
+        //  console.log(allSearchedBtns[i]);
         if (allSearchedBtns[i].length === 0) continue;
         let splitByBtn2 = allSearchedBtns[i].split(")</script>");
-      //  console.log(splitByBtn2[0]);
+        //  console.log(splitByBtn2[0]);
         if (splitByBtn2.length < 2) continue;
         let newUrl = splitByBtn2[0].split("\", \"");
-		//console.log("\r\n\r\n*******\r\nName :" + newUrl[1] + "\r\n\r\n");
-		let origLinkText = newUrl[1];
+        //console.log("\r\n\r\n*******\r\nName :" + newUrl[1] + "\r\n\r\n");
+        let origLinkText = newUrl[1];
         newUrl = newUrl[2].split("\"")[0];
 
         if (newUrl.length !== 0) {
@@ -94,11 +101,12 @@ const parseAllUrlds = (documentText) => {
 
     parseAllUrldsCounter--;
 
-	if (parseAllUrldsCounter == 0)
-    for (let i = 0; i < loadTreads.length; i++) {
-        // console.log(loadTreads[i][1]);
-        getDocumentByUrl(loadTreads[i][1], insertDocument, {threadNum: i});
-    }
+    if (parseAllUrldsCounter == 0)
+        for (let i = 0; i < loadTreads.length; i++) {
+            //console.log(loadTreads[i][1]);
+            getDocumentByUrl(loadTreads[i][1], insertDocument, { threadNum: i });
+            //console.log(window.location.href + encodeURI(loadTreads[i][1].replace(/%20/g, '+')) + "\r\n");
+        }
 }
 
 const insertDocument = (document, threadNumObj) => {
@@ -116,9 +124,9 @@ const searchReady = () => {
 
 let parseAllUrldsCounter = 0;
 function startAll() {
-	parseAllUrldsCounter++;
+    parseAllUrldsCounter++;
     getDocumentByUrl("https://reshenie-soft.ru/doc/left_1.htm", parseAllUrlds, {});
-	parseAllUrldsCounter++;
+    parseAllUrldsCounter++;
     getDocumentByUrl("https://reshenie-soft.ru/doc/left_2.htm", parseAllUrlds, {});
 }
 
@@ -126,11 +134,40 @@ function getDocumentByUrl(url, callback, argsOb) {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
-            // console.log("\r\n" + url + "\r\n");
+            // console.log( "\r\n" + url + "\r\n");
             callback(xhr.response, argsOb);
         }
     }
     xhr.responseType = "document";
     xhr.open('GET', url, true);
     xhr.send('');
+}
+
+input.onkeydown = function runScript(event) {
+    if (event.which == 13 || event.keyCode == 13) {
+        onSearchClick(input.value);
+        return false;
+    }
+    return true;
+}
+
+searchPanel.onmouseover = function (event) {
+    let target = event.target;
+    // target.style.background = '#accfe7';
+    answerBlock.style.display = 'block';
+    answerBlock.style.opacity = '1';
+    event.scrollTop = event.scrollHeight;
+}
+searchPanel.onmouseout = function (event) {
+    let target = event.target;
+    // target.style.background = '#accfe7';
+    answerBlock.style.opacity = '0.5';
+    event.scrollTop = event.scrollHeight;
+}
+window.onkeydown = function (e) {
+    let target = e.target
+    if (e.which == 27 || e.keyCode == 27) {
+        answerBlock.style.display = 'none';
+    }
+    return true;
 }
