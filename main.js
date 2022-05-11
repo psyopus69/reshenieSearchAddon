1bf953fe517dbff9aad247778fd7d71bd2326c42
Тарадайник Алексей Сергеевич <taradainik@local.reshenie.spb.ru>
2022-05-11 10:32:55 +0300

    Update ver

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ let searchBtn = document.createElement('input');
 let answerDiv = document.createElement('div');
 let answerBlock = document.createElement('div');
 let clearBtn = document.createElement('input');
-const input = document.createElement("input");
+const input = document.createElement('input');
+let iframe = document.createElement('iframe');
+let divFrame = document.createElement('div');
 
 let loadTreads = [];
 let loadThreadsCounter = 0;
@@ -13,7 +15,6 @@ document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
     searchPanel.setAttribute("id", "mainSearchPanel");
-
     searchBtn.setAttribute("id", "mainSearchBtn");
     searchBtn.type = 'button';
     searchBtn.value = "Поиск";
@@ -22,18 +23,23 @@ function ready() {
     clearBtn.setAttribute("id", "clearBtn");
     clearBtn.type = 'button';
     clearBtn.value = "X";
+    clearBtn.title = "Очистить/вернуться к ручному поиску";
     clearBtn.addEventListener("click", () => { onClear(); });
 
-    answerBlock.innerHTML = "Результаты поиска:";
+
     answerBlock.setAttribute("id", "answer");
-
     answerBlock.appendChild(answerDiv);
+    answerDiv.setAttribute('class', 'block-answer');
 
-
+    divFrame.className = 'block-frame';
+    divFrame.appendChild(iframe);
+    iframe.name = "iframe_link";
+    iframe.className = "frame-block"
 
     searchBtn.addEventListener("click", () => { onSearchClick(input.value); });
     searchBtn.classList.add("searchBlock");
     input.setAttribute("id", "mainInput");
+    input.setAttribute("placeholder", "Введите что-нибудь");
     input.type = "text";
     input.name = "SEARCH_INPUT";
     searchPanel.appendChild(searchBtn);
@@ -41,8 +47,12 @@ function ready() {
     searchPanel.appendChild(input);
     searchPanel.appendChild(answerBlock);
 
+
+
     const elements = document.getElementsByTagName("*");
+    elements[0].prepend(divFrame);
     elements[0].prepend(searchPanel);
+
     startAll();
 }
 
@@ -50,21 +60,47 @@ function ready() {
 
 function onClear() {
     answerDiv.innerHTML = "";
+    answerBlock.innerHTML = "";
     input.value = "";
+    answerBlock.style.paddingBottom = '0';
+    divFrame.style.display = 'none';
+    iframe.style.display = 'none';
 }
 
 function onSearchClick(substring) {
     if (!enableSearch) return;
     let counter = loadTreads.length;
+    answerBlock.innerHTML = "";
+    let x = 0;
     answerDiv.innerHTML = "";
+    answerBlock.style.paddingBottom = '0';
     //substring = substring.replaceAll(" ", "%20");
     for (let i = 0; i < loadTreads.length; i++) {
         // console.log("\r\n\r\nURL: " + loadTreads[i][1] + "\r\n" + substring.toUpperCase() + "  \r\nIN (i: " + i + ")\r\n " + loadTreads[i][2].toUpperCase());
+
         if (loadTreads[i][3].toUpperCase().includes(substring.toUpperCase())) {
             //console.log("нашлось! " + substring + " по адресу" + loadTreads[i][1] + ", orig: " + loadTreads[i][2]);
-            answerDiv.innerHTML += '<div class="block-link"><a class="link" href="'+window.location.href + encodeURI(loadTreads[i][1].replace(/%20/g, '+')) + "\r\n" + '"target="_blank">' + loadTreads[i][2] + ' (' + loadTreads[i][1] + ')</a></div>';
+            x += 1;
+            if (x == 1) {
+                answerBlock.innerHTML += "<p>Результаты поиска:</p>";
+                answerBlock.style.paddingBottom = '20px';
+                //iframe с первой найденной страницей
+            }
+            answerDiv.innerHTML += '<div class="block-link"><a class="link" href="' + window.location.href + encodeURI(loadTreads[i][1].replace(/%20/g, '+')) + "\r\n" + '"target="iframe_link">' + loadTreads[i][2] + ' (' + loadTreads[i][1] + ')</a></div>';
             answerBlock.appendChild(answerDiv);
+            if (x == 1) {
+                divFrame.style.display = 'block';
+                iframe.style.display = "block";
+                iframe.setAttribute("src", window.location.href + encodeURI(loadTreads[i][1].replace(/%20/g, '+')) + "\r\n")
+
+            }
         }
+    }
+    if (x == 0) {
+        answerBlock.innerHTML += "<p>По запросу <b>" + substring + "</b> ничего не найдено.</p>";
+        answerBlock.style.paddingBottom = '0';
+        // iframe.style.display = "none";
+
     }
 
 }
@@ -121,6 +157,9 @@ const searchReady = () => {
     searchBtn.classList.remove("searchBlock");
     searchBtn.classList.add("searchReady");
 };
+const openFrame = () => {
+
+}
 
 let parseAllUrldsCounter = 0;
 function startAll() {
@@ -142,6 +181,11 @@ function getDocumentByUrl(url, callback, argsOb) {
     xhr.open('GET', url, true);
     xhr.send('');
 }
+input.oninput = function changeInp() {
+    onSearchClick(input.value);
+    answerBlock.style.display = "block";
+    // xpathResult = document.evaluate("//body//text()[contains(., 'ЭЦП')]", document, null, XPathResult.ANY_TYPE, null);
+}
 
 input.onkeydown = function runScript(event) {
     if (event.which == 13 || event.keyCode == 13) {
@@ -152,22 +196,28 @@ input.onkeydown = function runScript(event) {
 }
 
 searchPanel.onmouseover = function (event) {
-    let target = event.target;
-    // target.style.background = '#accfe7';
     answerBlock.style.display = 'block';
-    answerBlock.style.opacity = '1';
     event.scrollTop = event.scrollHeight;
 }
 searchPanel.onmouseout = function (event) {
-    let target = event.target;
-    // target.style.background = '#accfe7';
-    answerBlock.style.opacity = '0.5';
+    answerBlock.style.display = 'none';
     event.scrollTop = event.scrollHeight;
 }
 window.onkeydown = function (e) {
-    let target = e.target
     if (e.which == 27 || e.keyCode == 27) {
         answerBlock.style.display = 'none';
+        divFrame.style.display = 'none';
+        iframe.style.display = 'none';
+
     }
     return true;
 }
+divFrame.onclick = function (event) {
+    divFrame.style.display = 'none';
+}
+document.addEventListener("click", (e) => {
+    target = e.target
+    if (target.className == 'link') {
+        divFrame.style.display = 'block';
+    }
+});
