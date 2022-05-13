090f112ceb112eaad558426618ad3de35f2cdd02
polovinalam <aleshka_hax@mail.ru>
2022-05-13 09:29:11 +0300

    Update ver

--- main.js
+++ main.js
@@ -19,13 +19,11 @@ function ready() {
     searchBtn.type = 'button';
     searchBtn.value = "Поиск";
 
-
     clearBtn.setAttribute("id", "clearBtn");
     clearBtn.type = 'button';
     clearBtn.value = "X";
     clearBtn.title = "Очистить/вернуться к ручному поиску";
     clearBtn.addEventListener("click", () => { onClear(); });
-
 
     answerBlock.setAttribute("id", "answer");
     answerBlock.appendChild(answerDiv);
@@ -34,7 +32,7 @@ function ready() {
     divFrame.className = 'block-frame';
     divFrame.appendChild(iframe);
     iframe.name = "iframe_link";
-    iframe.className = "frame-block"
+    iframe.className = "block-frame__iframe"
 
     searchBtn.addEventListener("click", () => { onSearchClick(input.value); });
     searchBtn.classList.add("searchBlock");
@@ -47,24 +45,22 @@ function ready() {
     searchPanel.appendChild(input);
     searchPanel.appendChild(answerBlock);
 
-
-
     const elements = document.getElementsByTagName("*");
     elements[0].prepend(divFrame);
     elements[0].prepend(searchPanel);
 
     startAll();
 }
-
-
+const n = 'none';
+const b = 'block';
 
 function onClear() {
     answerDiv.innerHTML = "";
     answerBlock.innerHTML = "";
     input.value = "";
     answerBlock.style.paddingBottom = '0';
-    divFrame.style.display = 'none';
-    iframe.style.display = 'none';
+    divFrame.style.display = n;
+    iframe.style.display = n;
 }
 
 function onSearchClick(substring) {
@@ -76,23 +72,21 @@ function onSearchClick(substring) {
     answerBlock.style.paddingBottom = '0';
     //substring = substring.replaceAll(" ", "%20");
     for (let i = 0; i < loadTreads.length; i++) {
-        // console.log("\r\n\r\nURL: " + loadTreads[i][1] + "\r\n" + substring.toUpperCase() + "  \r\nIN (i: " + i + ")\r\n " + loadTreads[i][2].toUpperCase());
+        //  console.log("\r\n\r\nURL: " + loadTreads[i][1] + "\r\n" + substring.toUpperCase() + "  \r\nIN (i: " + i + ")\r\n " + loadTreads[i][2].toUpperCase());
 
         if (loadTreads[i][3].toUpperCase().includes(substring.toUpperCase())) {
-            //console.log("нашлось! " + substring + " по адресу" + loadTreads[i][1] + ", orig: " + loadTreads[i][2]);
+            console.log("нашлось! " + substring + " по адресу" + loadTreads[i][1] + ", orig: " + loadTreads[i][2]);
             x += 1;
             if (x == 1) {
                 answerBlock.innerHTML += "<p>Результаты поиска:</p>";
                 answerBlock.style.paddingBottom = '20px';
-                //iframe с первой найденной страницей
             }
             answerDiv.innerHTML += '<div class="block-link"><a class="link" href="' + window.location.href + encodeURI(loadTreads[i][1].replace(/%20/g, '+')) + "\r\n" + '"target="iframe_link">' + loadTreads[i][2] + ' (' + loadTreads[i][1] + ')</a></div>';
             answerBlock.appendChild(answerDiv);
             if (x == 1) {
-                divFrame.style.display = 'block';
-                iframe.style.display = "block";
+                divFrame.style.display = b;
+                iframe.style.display = b;
                 iframe.setAttribute("src", window.location.href + encodeURI(loadTreads[i][1].replace(/%20/g, '+')) + "\r\n")
-
             }
         }
     }
@@ -105,24 +99,28 @@ function onSearchClick(substring) {
 
 }
 
-
 const parseAllUrlds = (documentText) => {
     documentText = documentText.getElementsByTagName("td");
-    //console.log(documentText);
+    // console.log(documentText);
     let massHtml = "";
     for (let i = 0; i < documentText.length; i++) {
-        if (documentText[i].innerHTML.includes("-->")) continue;
-        massHtml += documentText[i].innerHTML;
-        //console.log(documentText[i].innerHTML);
+        let elem = documentText[i].innerHTML
+        //убираем все комментарии в элементе 
+        while (elem.search("<!--") > 0) {
+            elem = elem.replace(elem.slice(elem.search("<!--"), elem.search("-->") + 3), '');
+        }
+
+        if (elem.includes("<tbody>")) continue;
+        massHtml += elem;
     }
     let str = "<script>";
     const allSearchedBtns = massHtml.split(str + "btnL");
     // console.log(allSearchedBtns);
     for (let i = 0; i < allSearchedBtns.length; i++) {
-        //  console.log(allSearchedBtns[i]);
+        //   console.log(allSearchedBtns[i]);
         if (allSearchedBtns[i].length === 0) continue;
         let splitByBtn2 = allSearchedBtns[i].split(")</script>");
-        //  console.log(splitByBtn2[0]);
+        //   console.log(splitByBtn2[0]);
         if (splitByBtn2.length < 2) continue;
         let newUrl = splitByBtn2[0].split("\", \"");
         //console.log("\r\n\r\n*******\r\nName :" + newUrl[1] + "\r\n\r\n");
@@ -157,9 +155,6 @@ const searchReady = () => {
     searchBtn.classList.remove("searchBlock");
     searchBtn.classList.add("searchReady");
 };
-const openFrame = () => {
-
-}
 
 let parseAllUrldsCounter = 0;
 function startAll() {
@@ -173,7 +168,7 @@ function getDocumentByUrl(url, callback, argsOb) {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
-            // console.log( "\r\n" + url + "\r\n");
+            //  console.log( "\r\n" + url + "\r\n");
             callback(xhr.response, argsOb);
         }
     }
@@ -181,43 +176,70 @@ function getDocumentByUrl(url, callback, argsOb) {
     xhr.open('GET', url, true);
     xhr.send('');
 }
+//событие при изменении input
 input.oninput = function changeInp() {
     onSearchClick(input.value);
+    searchIframe(iframe, input.value.trim())
     answerBlock.style.display = "block";
-    // xpathResult = document.evaluate("//body//text()[contains(., 'ЭЦП')]", document, null, XPathResult.ANY_TYPE, null);
 }
-
-input.onkeydown = function runScript(event) {
-    if (event.which == 13 || event.keyCode == 13) {
+//поиск при нажатии клавиши ENTER
+input.onkeydown = function runScript(e) {
+    if (e.which == 13 || e.keyCode == 13) {
         onSearchClick(input.value);
         return false;
     }
     return true;
 }
-
-searchPanel.onmouseover = function (event) {
-    answerBlock.style.display = 'block';
-    event.scrollTop = event.scrollHeight;
+//отображение списка найденных результатов при наведении
+searchPanel.onmouseover = function (e) {
+    answerBlock.style.display = b;
+    e.scrollTop = e.scrollHeight;
 }
-searchPanel.onmouseout = function (event) {
-    answerBlock.style.display = 'none';
-    event.scrollTop = event.scrollHeight;
+//скрытие списка найденных результатов при отводе курсора от элемента
+searchPanel.onmouseout = function (e) {
+    answerBlock.style.display = n;
+    e.scrollTop = e.scrollHeight;
 }
+//скрытие элементов при нажатии ESC
 window.onkeydown = function (e) {
     if (e.which == 27 || e.keyCode == 27) {
-        answerBlock.style.display = 'none';
-        divFrame.style.display = 'none';
-        iframe.style.display = 'none';
-
+        answerBlock.style.display = n;
+        divFrame.style.display = n;
+        iframe.style.display = n;
     }
     return true;
 }
-divFrame.onclick = function (event) {
-    divFrame.style.display = 'none';
+//скрытие блокирующего элемента под фремом(модального окна) при нажатии
+divFrame.onclick = function (e) {
+    divFrame.style.display = n;
 }
+//при клике по ссылкам поиска
 document.addEventListener("click", (e) => {
     target = e.target
     if (target.className == 'link') {
-        divFrame.style.display = 'block';
+        divFrame.style.display = b;
+        frame = document.getElementsByTagName('iframe')[0];
+        searchIframe(frame, input.value.trim());
+        // let idoc = iframe.contentDocument || iframe.contentWindow.document;
     }
 });
+const searchIframe = (frame, text) => {
+    textUpper = text.toUpperCase();
+    frame.onload = function () {
+        let iframeDoc = frame.contentWindow.document;
+        // iframeDoc.body.style.backgroundColor = 'red';
+        elem = iframeDoc.getElementsByTagName('p');
+        // console.log(elem);
+        for (let i = 0; i < elem.length; i++) {
+            // console.log(elem[i].innerText);
+            if (elem[i].innerText.toUpperCase().includes(textUpper)) {
+                console.log('Нашёл ' + elem[i].outerHTML);
+                if (textUpper !== '' && textUpper.length > 1) {
+                    elem[i].style.backgroundColor = '#fcfc6d';
+                }
+            }
+        }
+
+    }
+
+}
